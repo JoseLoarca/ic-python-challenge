9c5033503bbd1365854daf7caa2a1cffde69f9e8
José Loarca <joseloarca97@icloud.com>
2022-08-30 18:11:55 -0600

    added missing comments

--- lambda_demo.py
+++ lambda_demo.py
@@ -1,7 +1,7 @@
 def extract_even_integers(integers):
     """
     Extracts even integers from a list of integers. Is 0 an even integer?
-    :param integers:
-    :return:
+    :param integers: list of integers
+    :return: list of even integers
     """
     return [x for x in integers if (lambda x: x % 2 == 0)(x)]
